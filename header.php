a7465b08217d54b6264994d4dc3826b7dc1e1a6d
root <root@tomcant.(none)>
2013-09-15 00:10:01 +0000

    Content changes.

--- header.php
+++ header.php
@@ -2,4 +2,6 @@
   <?php require_once 'nav.php'; ?>
 </div>
 
-<div id="heading"><h1>Hello, I'm <strong>Tom</strong> <span class="ampersand">&</span> I'm a <em>developer</em></h1></div>
+<div id="heading">
+  <h1>Hello. I'm an <strong><em>engineer</em></strong>.</h1>
+</div>
