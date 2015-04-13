52c16ffc1b4fa529e80a8966560e39ee45eaec40
Tom Cant <tomcant@gmail.com>
2015-04-13 22:19:44 +0000

    Added contact form.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Enquiry;
+use AppBundle\Form\EnquiryType;
 
 class DefaultController extends Controller
 {
@@ -18,8 +21,32 @@ class DefaultController extends Controller
     /**
      * @Route("/contact", name="contact")
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('AppBundle:Default:contact.html.twig');
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(new EnquiryType(), $enquiry, array(
+            'action' => $this->generateUrl('contact'),
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // Save the enquiry to the database.
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($enquiry);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Thank you for your message. I will get back to you as soon as possible.'
+            );
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('AppBundle:Default:contact.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/AppBundle/Entity/Enquiry.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+
+/**
+ * Enquiry
+ *
+ * @ORM\Table(name="enquiry")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Enquiry
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Enquiry
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Enquiry
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Enquiry
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Enquiry
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        $this->setCreated(new \DateTime());
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata
+            ->addPropertyConstraint('name', new NotBlank())
+            ->addPropertyConstraint('email', new Email())
+            ->addPropertyConstraint('message', new NotBlank())
+        ;
+    }
+}
