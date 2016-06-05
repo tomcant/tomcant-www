70b56c9fa856f659ef420d11e232319832c92c4c
Tom Cant <tomcant@gmail.com>
2016-06-05 23:12:26 +0100

    apt-get update before attempting to install packages.

--- chef/cookbooks/tomcant/recipes/default.rb
+++ chef/cookbooks/tomcant/recipes/default.rb
@@ -1,10 +1,11 @@
+include_recipe 'tomcant::apt'
+include_recipe 'fb_apt'
+
 %w{htop git vim}.each do |package|
   package package do
     action :install
   end
 end
 
-include_recipe 'tomcant::apt'
-include_recipe 'fb_apt'
 include_recipe 'tomcant::nginx'
 include_recipe 'tomcant::php'
