ca8e5ad662318b97b320e56e65b0102797a276a8
Tom Cant <tomcant@gmail.com>
2016-06-05 01:49:57 +0100

    Provisioned nginx configuration.

--- chef/cookbooks/tomcant/recipes/nginx.rb
+++ chef/cookbooks/tomcant/recipes/nginx.rb
@@ -5,3 +5,16 @@ package 'nginx'
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true
 end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+  source 'nginx/nginx.conf'
+end
+
+file '/etc/nginx/conf.d/default.conf' do
+  action :nothing
+  subscribes :delete, 'package[nginx]'
+end
+
+cookbook_file '/etc/nginx/conf.d/tomcant.vhost' do
+  source 'nginx/conf.d/tomcant.dev.vhost'
+end
